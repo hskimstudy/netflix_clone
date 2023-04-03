ab5701c24e122a96b617541e6b3e7697cb37b364
hskimstudy <hskimstudy@gmail.com>
2023-04-03 11:52:45 +0900

    +useLocation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import "./Nav.css"
+import { useNavigate } from 'react-router-dom';
 
 export const Nav = () => {
 
   const [show, setShow] = useState(false);
+  const [searchValue, setSearchValue] = useState("")
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -20,6 +23,12 @@ export const Nav = () => {
   }, []);
 
 
+  const handleChange = (e) => {
+    setSearchValue(e.target.value)
+    navigate(`/search?q=${e.target.value}`)
+  }
+
+
   return (
     <nav className={`nav ${show && 'nav__black'}`}>
       <img
@@ -28,6 +37,10 @@ export const Nav = () => {
         className='nav__logo'
         onClick={() => window.location.reload()}
       />
+
+      <input value={searchValue} onChange={handleChange} className='nav__input' type='text' placeholder='영화를 검색해주세요' />
+
+
       <img
         alt='User logged'
         src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,7 +1,44 @@
-import React from 'react'
+import { useLocation } from 'react-router-dom';
+import axios from '../../api/axios';
+import React, { useEffect, useState } from 'react'
 
 export default function SearchPage() {
+
+  const [searchResults, setSearchResults] = useState([])
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  }
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm)
+    }
+  }, [searchTerm])
+
+  const fetchSearchMovie = async (searchTerm) => {
+    console.log("searchTerm", searchTerm);
+    try {
+
+      const request = await axios.get(`/search/multi?include_adult=false&query=${searchTerm}`)
+      console.log(request)
+      setSearchResults(request.data.results)
+
+    } catch (error) {
+      console.log("error", error)
+    }
+    return <div></div>
+  }
+
+
+
+
+
   return (
-    <div>searc</div>
+    <div></div>
   )
 }
